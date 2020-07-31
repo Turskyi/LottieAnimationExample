ec04ec698a3bb8f12e87a1bd7ceba85125219e38
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-07-31 18:55:29 +0300

    Demonstration of all sorts of cases used with animation.

--- app/src/main/java/io/github/turskyi/lottieanimationexample/MainActivity.kt
+++ app/src/main/java/io/github/turskyi/lottieanimationexample/MainActivity.kt
@@ -1,19 +1,122 @@
 package io.github.turskyi.lottieanimationexample
 
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
+import android.widget.SeekBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 @SuppressLint("ClickableViewAccessibility")
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initView()
+        initListener()
+    }
 
-        var isStarted = false
+    private fun initView() {
+        animViewSpeed.speed = (seekBarSpeed.progress.toFloat() / 50) + 1
+        animViewPingPong.playAnimation()
+        animViewOnCompletion.progress = 1f
+    }
 
+    private fun initListener() {
+        onClick()
+        onTap()
+        onSpeedChange()
+        playBackwards()
+        forwardBackwardLoop()
+        btnStart.setOnClickListener {
+            animViewOnCompletion.playAnimation()
+        }
+        animViewOnCompletion.addAnimatorListener(object : AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                Log.e("Animation:", "start")
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                Log.e("Animation:", "end")
+                Toast.makeText(applicationContext, "Animation Completed", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                Log.e("Animation:", "cancel")
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+                Log.e("Animation:", "repeat")
+            }
+        })
+    }
+
+    private fun forwardBackwardLoop() {
+        animViewPingPong.addAnimatorListener(object : AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                Log.e("Animation:", "start")
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                Log.e("Animation:", "end")
+                if (animViewPingPong.speed > 0) {
+                    animViewPingPong.speed = -1f
+                    animViewPingPong.playAnimation()
+                } else {
+                    animViewPingPong.speed = 1f
+                    animViewPingPong.playAnimation()
+                }
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                Log.e("Animation:", "cancel")
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+                Log.e("Animation:", "repeat")
+            }
+        })
+    }
+
+    private fun playBackwards() {
+        btnReverse.setOnClickListener {
+            if (animViewBackward.speed > 0) {
+                btnReverse.text = getString(R.string.play_forward)
+                animViewBackward.speed = -1f
+            } else {
+                animViewBackward.speed = 1f
+                btnReverse.text = resources.getString(R.string.play_backward)
+            }
+        }
+    }
+
+    private fun onSpeedChange() {
+        seekBarSpeed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                run {
+                    animViewSpeed.speed = (progress.toFloat() / 50) + 1
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
+        })
+    }
+
+    private fun onClick() {
+        animViewClick.setOnClickListener {
+            animViewClick.playAnimation()
+        }
+    }
+
+    private fun onTap() {
         animViewFlight.setOnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
@@ -21,7 +124,21 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                     return@setOnTouchListener true
                 }
                 MotionEvent.ACTION_UP -> {
-                    isStarted = true
+                    animViewFlight.pauseAnimation()
+                    return@setOnTouchListener true
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    animViewFlight.pauseAnimation()
+                    return@setOnTouchListener true
+                }
+
+                MotionEvent.ACTION_BUTTON_RELEASE -> {
+                    animViewFlight.pauseAnimation()
+                    return@setOnTouchListener true
+                }
+
+                MotionEvent.ACTION_SCROLL -> {
                     animViewFlight.pauseAnimation()
                     return@setOnTouchListener true
                 }
